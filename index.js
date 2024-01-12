18f84ab6b6944226a51f9afa1ed59c2228c14433
gabodani <153448099+gabodani@users.noreply.github.com>
2024-01-12 13:39:22 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,299 @@
+const telegrambot=require('node-telegram-bot-api');
+const token='6602986941:AAHv-k8CpaGXsSrrVqchRBA0Joc5MYg2Xwo';
+const bot=new telegrambot(token,{polling:true});
+//talk
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+ bot.sendMessage(chatId, 'გამარჯობა მე ვარ ზვიადა,რით შემიძლია დაგეხმარო?.');
+  });
+bot.onText(/შენი საყვარელი საჭმელი/,(msg)=>{
+  const food=msg.chat.id;
+  bot.sendMessage(food,`რათქმაუნდა ქართული მამა-პაპური ხინკალი`);
+})
+  //CHANNELS
+  bot.onText(/ჩართე იმედი/,(msg)=>{
+    const imedi=msg.chat.id;
+    bot.sendMessage(imedi,`https://tv.cdn.xsg.ge/imedihd/tracks-v1a1/mono.m3u8`);
+  })
+  bot.onText(/ჩართე პალიტრა/,(msg)=>{
+    const palitra=msg.chat.id;
+    bot.sendMessage(palitra,`https://live.palitranews.ge/hls/palitratv/index.m3u8`);
+  })
+  //MUSIC
+  bot.onText(/ლექსსენი ხელები მაღლა/,(msg)=>{
+    const simg=msg.chat.id;
+    bot.sendMessage(simg,`https://youtu.be/LBwZFFRCm0Y?si=30p4D7M2Vxcr69Zi`);
+  })
+  bot.onText(/Loudspeakers world in my eyes/,(msg)=>{
+    const loud=msg.chat.id;
+    bot.sendMessage(loud,`https://youtu.be/im93RBMcuXc?si=Iwx76SM5HI99UWO3`);
+  })
+  bot.onText(/Last christmas/,(msg)=>{
+    const wham=msg.chat.id;
+    bot.sendMessage(wham,`https://youtu.be/E8gmARGvPlI?si=KceiwLzjhgbuDdcU`);
+  })
+  bot.onText(/Baat it/,(msg)=>{
+    const beat=msg.chat.id;
+    bot.sendMessage(beat,`https://youtu.be/E8gmARGvPlI?si=KceiwLzjhgbuDdcU`);
+  })
+  bot.onText(/Billie jean/,(msg)=>{
+    const billie=msg.chat.id;
+    bot.sendMessage(billie,`https://youtu.be/Zi_XLOBDo_Y?si=o18NTwkE8GsP_Cni`);
+  })
+  bot.onText(/In da club/,(msg)=>{
+    const ind=msg.chat.id;
+    bot.sendMessage(ind,`https://youtu.be/5qm8PH4xAss?si=w6WZDAp4VqKSCeRj`);
+  })
+  bot.onText(/მგზავრები მთვარე/,(msg)=>{
+    const mt=msg.chat.id;
+    bot.sendMessage(mt,`https://youtu.be/ofak-1UW5es?si=_cMM0ED9rmqjti2S`);
+  })
+  bot.onText(/ანრი ჯოხაძე უფლება/,(msg)=>{
+    const anr=msg.chat.id;
+    bot.sendMessage(anr,`https://youtu.be/5LXKaRwFpRw?si=agRDGTdJodURstlZ`);
+  })
+  bot.onText(/არა კი/,(msg)=>{
+    const ki=msg.chat.id;
+    bot.sendMessage(ki,`https://youtu.be/pXWzLCz8dK8?si=d67IL7Sl0KCMwolW`);
+  })
+  bot.onText(/Actions of directions/,(msg)=>{
+    const act=msg.chat.id;
+    bot.sendMessage(act,`https://youtu.be/mCcn60Bq1-I?si=zAttr4t-hwo4iN30`);
+  })
+  bot.onText(/I want to break free/,(msg)=>{
+    const want=msg.chat.id;
+    bot.sendMessage(want,`https://youtu.be/WUOtCLOXgm8?si=mTor3vx-2JEJZ8a8`);
+  })
+  bot.onText(/Cheri cheri lady/,(msg)=>{
+    const chari=msg.chat.id;
+    bot.sendMessage(cheri,`https://youtu.be/eNvUS-6PTbs?si=59A0AnABbZ2qD0yp`);
+  })
+  bot.onText(/ქართული დისკო/,(msg)=>{
+    const geo=msg.chat.id;
+    bot.sendMessage(geo,`https://youtu.be/AMqUHPqCbI4?si=GUUClt4_h7nH4ngC`);
+  })
+  bot.onText(/I will survive/,(msg)=>{
+    const w=msg.chat.id;
+    bot.sendMessage(w,`https://youtu.be/6dYWe1c3OyU?si=P9Ww3bD3ONInKIo5`);
+  })
+  bot.onText(/Snowman/,(msg)=>{
+    const sia=msg.chat.id;
+    bot.sendMessage(sia,`https://youtu.be/J_QGZspO4gg?si=98Zm59fM1eutJSGN`);
+  })
+  bot.onText(/Macarena/,(msg)=>{
+    const makaroni=msg.chat.id;
+    bot.sendMessage(makaroni,`https://youtu.be/2zvd1JQ0EKY?si=I5dU_MTlPkp4INEB`);
+  })
+  bot.onText(/Blinding lights/,(msg)=>{
+    const wee=msg.chat.id;
+    bot.sendMessage(wee,`https://youtu.be/4NRXx6U8ABQ?si=x5bn_Tc4px6bRCie`);
+  })
+  bot.onText(/Save your tears/,(msg)=>{
+    const tear=msg.chat.id;
+    bot.sendMessage(tear,`https://youtu.be/XXYlFuWEuKI?si=cDfR-4Q2AphOqIgK`);
+  })
+  bot.onText(/არა მსხალი/,(msg)=>{
+  const msx=msg.chat.id;
+  bot.sendMessage(msx,`https://youtu.be/dTU7dvzLRZA?si=JWAoKeMOUg4_wB4s`);
+  })
+  bot.onText(/არა ირმა/,(msg)=>{
+    const irma=msg.chat.id;
+    bot.sendMessage(irma,`https://youtu.be/TVBVblcj46g?si=mmUuaAuzhD81hM0L`);
+  })
+  bot.onText(/არა საქართველო/,(msg)=>{
+    const saq=msg.chat.id;
+    bot.sendMessage(saq,`https://youtu.be/N32Ew_68oBQ?si=p1tCxFnuxVfDhRff`);
+  })
+  bot.onText(/Den den duri/,(msg)=>{
+    const den=msg.chat.id;
+    bot.sendMessage(den,`https://youtu.be/ismxF7Yxugc?si=C9fGXWNOqwyWaman`);
+  })
+  bot.onText(/დამისხი დამალევინე/,(msg)=>{
+    const mgzavrebi=msg.chat.id;
+    bot.sendMessage(mgzavrebi,`https://youtu.be/nC6F_54dDss?si=UoFZxH0ieRyiP4Zw`);
+  })
+  bot.onText(/Reggaeon mdevari/,(msg)=>{
+    const reggaeon=msg.chat.id;
+    bot.sendMessage(reggaeon,`https://youtu.be/w7kUCHC8ywA?si=1piJ3apRbKddiiaa`);
+  })
+  bot.onText(/Michael jackson bad/,(msg)=>{
+    const maikli=msg.chat.id;
+    bot.sendMessage(maikli,`https://youtu.be/dsUXAEzaC3Q?si=EPV3kAs5Qmn3N2fr`);
+  })
+  bot.onText(/Smooth criminal/,(msg)=>{
+    const mkj=msg.chat.id;
+    bot.sendMessage(mkj,`https://youtu.be/sFvENQBc-F8?si=SzASZwyQRkZL4M-q`);
+  })
+  bot.onText(/ზვიად ბექაური აღარ მოხვიდე/,(msg)=>{
+    const zviki=msg.chat.id;
+    bot.sendMessage(zviki,`https://youtu.be/nOcJ1fIpfHY?si=Z0OPvSFGpujIWLuh`);
+  })
+  bot.onText(/ანანკე/,(msg)=>{
+    const ananke=msg.chat.id;
+    bot.sendMessage(ananke,`https://youtu.be/FyMdNTv3bl4?si=fA3_zWBl2mFBryQP`);
+  })
+  bot.onText(/505/,(msg)=>{
+    const xutasxuti=msg.chat.id;
+    bot.sendMessage(xutasxuti,`https://youtu.be/qU9mHegkTc4?si=6NNEfh161xcTbq3f`);
+  })
+  bot.onText(/Arabella/,(msg)=>{
+    const arabella=msg.chat.id;
+    bot.sendMessage(arabella,`https://youtu.be/Jn6-TItCazo?si=qjOpdNUQ9wpkrZfE`);
+  })
+  bot.onText(/Mgmt little dark age/,(msg)=>{
+    const mgmt=msg.chat.id;
+    bot.sendMessage(mgmt,`https://youtu.be/rtL5oMyBHPs?si=ALJf64QwQWkPsNcM`);
+  })
+  //QUESTIONS
+  bot.onText(/ვინ იყო უცხო მოყმე/,(msg)=>{
+    const moyme=msg.chat.id;
+    bot.sendMessage(moyme,`ტარიელი`);
+  })
+  bot.onText(/ვინ დაწერა ვეფხისტყაოსანი/,(msg)=>{
+    const d=msg.chat.id;
+    bot.sendMessage(d,`შოთა რუსთველმა`);
+  })
+  bot.onText(/რამდენი ასოა ქართულში/,(msg)=>{
+    const aso=msg.chat.id;
+    bot.sendMessage(aso,`33`);
+  })
+  bot.onText(/რამდენი ხმოვანია ქართულში/,(msg)=>{
+    const xmovani=msg.chat.id;
+    bot.sendMessage(xmovani,`5 ხმოვანი:ა,ე,ი,ო,უ,`);
+  })
+  bot.onText(/საქართველოს პირველი პრეზიდენტი/,(msg)=>{
+    const zvia=msg.chat.id;
+    bot.sendMessage(zvia,`ზვიად გამსახურდია`);
+  })
+  //CAPITAL CITY
+  bot.onText(/საფრანგეთის დედაქალაქი/,(msg)=>{
+    const france=msg.chat.id;
+    bot.sendMessage(france,`პარიზი`);
+  })
+  bot.onText(/ზიმბაბვეს დედაქალაქი/,(msg)=>{
+    const zimbabve=msg.chat.id;
+    bot.sendMessage(zimbabve,`ჰარარე`);
+  })
+  bot.onText(/ურუგვაის დედაქალაქი/,(msg)=>{
+    const uruguay=msg.chat.id;
+    bot.sendMessage(uruguay,`მონტევიდეო`);
+  })
+  bot.onText(/პაკისტანის დედაქალაქი/,(msg)=>{
+    const pakistan=msg.chat.id;
+    bot.sendMessage(pakistan,`ისლამაბადი`);
+  })
+  bot.onText(/საქართველოს დედაქალაქი/,(msg)=>{
+    const georgia=msg.chat.id;
+    bot.sendMessage(georgia,`თბილისი`);
+  })
+  bot.onText(/ყაზახეთის დედაქალაქი/,(msg)=>{
+    const kaz=msg.chat.id;
+    bot.sendMessage(kaz,`ასტანა`);  
+  })
+  bot.onText(/ნიგერიის დედაქალაქი/,(msg)=>{
+    const nigeria=msg.chat.id;
+    bot.sendMessage(nigeria,`აბუჯა`);
+  })
+  bot.onText(/მაროკოს დედაქალაქი/,(msg)=>{
+    const marocco=msg.chat.id;
+    bot.sendMessage(marocco,`რაბატი`);
+  })
+  bot.onText(/ეთიოპიის დედაქალაქი/,(msg)=>{
+    const etiopia=msg.chat.id;
+    bot.sendMessage(etiopia,`ადის-აბება`);
+  })
+  bot.onText(/ლაოსის დედაქალაქი/,(msg)=>{
+    const laos=msg.chat.id;
+    bot.sendMessage(laos,`ვიენტიანი`);
+  })
+  bot.onText(/ისრაელის დედაქალაქი/,(msg)=>{
+    const israel=msg.chat.id;
+    bot.sendMessage(israel,`იერუსალიმი`);
+  })
+  bot.onText(/რუსეთის დედაქალაქი/,(msg)=>{
+    const russia=msg.chat.id;
+    bot.sendMessage(russia,`მოსკოვი`);
+  })
+  bot.onText(/ბელიზის დედაქალაქი/,(msg)=>{
+    const belize=msg.chat.id;
+    bot.sendMessage(belize,`ბელმოპანი`);
+  })
+  bot.onText(/სომხეთის დედაქალაქი/,(msg)=>{
+    const armenia=msg.chat.id;
+    bot.sendMessage(armenia,`ერევანი`);
+  })
+  bot.onText(/პანამის დედაქალაქი/,(msg)=>{
+    const panama=msg.chat.id;
+    bot.sendMessage(panama,`პანამა`);
+  })
+  bot.onText(/აზერბაიჯანის დედაქალაქი/,(msg)=>{
+    const az=msg.chat.id;
+    bot.sendMessage(az,`აზერბაიჯანი`);
+  })
+  bot.onText(/ჩინეთის დედაქალაქი/,(msg)=>{
+    const china=msg.chat.id;
+    bot.sendMessage(china,`პეკინი`);
+  })
+  bot.onText(/ლატვიის დედაქალაქი/,(msg)=>{
+    const lat=msg.chat.id;
+    bot.sendMessage(lat,`რიგა`);
+  })
+  bot.onText(/თურქეთის დედაქალაქი/,(msg)=>{
+    const turkey=msg.chat.id;
+    bot.sendMessage(turkey,`ანკარა`);
+  })
+  bot.onText(/იტალიის დედაქალაქი/,(msg)=>{
+    const ita=msg.chat.id;
+    bot.sendMessage(ita,`რომი`);
+  })
+  bot.onText(/შვედეთის დედაქალაქი/,(msg)=>{
+    const swed=msg.chat.id;
+    bot.sendMessage(swed,`სტოკჰოლმი`);
+  })
+  bot.onText(/ფინეთის დედაქაალაქი/,(msg)=>{
+    const fin=msg.chat.id;
+    bot.sendMessage(fin,`ჰელსინკი`);
+  })
+  bot.onText(/ნორვეგიის დედაქალაქი/,(msg)=>{
+    const norw=msg.chat.id;
+    bot.sendMessage(norw,`ოსლო`);
+  })
+  bot.onText(/გერმანიის დედაქალაქი/,(msg)=>{
+    const ger=msg.chat.id;
+    bot.sendMessage(ger,`ბერლინი`);
+  })
+  //information
+  bot.onText(/ინფორმაცია ნაპოლეონ ბონაპარტზე/,(/მოძებნე ინფორმაცია ნაპოლეონ ბონაპარტზე/,(msg)=>{
+    const napoleon=msg.chat.id;
+    bot.sendMessage(napoleon,`https://ka.wikipedia.org/wiki/%E1%83%9C%E1%83%90%E1%83%9E%E1%83%9D%E1%83%9A%E1%83%94%E1%83%9D%E1%83%9C_%E1%83%91%E1%83%9D%E1%83%9C%E1%83%90%E1%83%9E%E1%83%90%E1%83%A0%E1%83%A2%E1%83%98`);
+  }))
+  bot.onText(/ნაპოლეონ ბონაპარტი/,(msg)=>{
+    const nap=msg.chat.id;
+    bot.sendMessage(nap,`https://ka.wikipedia.org/wiki/%E1%83%9C%E1%83%90%E1%83%9E%E1%83%9D%E1%83%9A%E1%83%94%E1%83%9D%E1%83%9C_%E1%83%91%E1%83%9D%E1%83%9C%E1%83%90%E1%83%9E%E1%83%90%E1%83%A0%E1%83%A2%E1%83%98`);
+  })
+  bot.onText(/მიქელანჯელო/,(msg)=>{
+    const mik=msg.chat.id;
+    bot.sendMessage(mik,`https://ka.wikipedia.org/wiki/%E1%83%9B%E1%83%98%E1%83%A5%E1%83%94%E1%83%9A%E1%83%90%E1%83%9C%E1%83%AF%E1%83%94%E1%83%9A%E1%83%9D_%E1%83%91%E1%83%A3%E1%83%9D%E1%83%9C%E1%83%90%E1%83%A0%E1%83%9D%E1%83%A2%E1%83%98`);
+  })
+  bot.onText(/ინფორმაცია მიქელანჯელოს შესახებ /,(msg)=>{
+    const mikk=msg.chat.id;
+    bot.sendMessage(mikk,`https://ka.wikipedia.org/wiki/%E1%83%9B%E1%83%98%E1%83%A5%E1%83%94%E1%83%9A%E1%83%90%E1%83%9C%E1%83%AF%E1%83%94%E1%83%9A%E1%83%9D_%E1%83%91%E1%83%A3%E1%83%9D%E1%83%9C%E1%83%90%E1%83%A0%E1%83%9D%E1%83%A2%E1%83%98`);
+  })
+  //recipes
+  bot.onText(/ხინკლის რეცეპტი/,(msg)=>{
+    const xink=msg.chat.id;
+    bot.sendMessage(xink,`https://gemrielia.ge/recipe/1117-mTiuluri-xinkali-sakuTari-xeliT/`);
+  })
+  bot.onText(/ხინკლის მომზადების წესი/,(msg)=>{
+    const xinka=msg.chat.id;
+    bot.sendMessage(xinka,`https://gemrielia.ge/recipe/1117-mTiuluri-xinkali-sakuTari-xeliT/`);
+  })
+  bot.onText(/პიცის რეცეპტი/,(msg)=>{
+    const pizza=msg.chat.id;
+    bot.sendMessage(pizza,`https://kulinaria.ge/receptebi/pica-saxlis-pirobebSi_209/`);
+  })
+  bot.onText(/პიცის მომზადების წესი/,(msg)=>{
+    const piza=msg.chat.id;
+    bot.sendMessage(piza,`https://kulinaria.ge/receptebi/pica-saxlis-pirobebSi_209/`);
+  })
+
